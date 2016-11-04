338b1b4a5e258209db8853e9a851838a88a41e2b
Karl Brodowsky <bk1@gmx.net>
2016-11-04 01:57:34 +0100

    complete, but never perfect

--- src/main/java/net/itsky/devoxx/wycocawr/DoubleInaccuracy.java
+++ src/main/java/net/itsky/devoxx/wycocawr/DoubleInaccuracy.java
@@ -2,4 +2,13 @@ package net.itsky.devoxx.wycocawr;
 
 public class DoubleInaccuracy {
 
+	public static void main(String[] args) {
+		double u = 1.0;
+		double v = u / 1e20;
+		double w = v + 1;
+		double x = w - 1;
+		double y = x * 1e20;
+		System.out.println("u=" + u + " v=" + v + " w=" + w + " x=" + x + " y=" + y + "\n");
+	}
+
 }
--- src/main/java/net/itsky/devoxx/wycocawr/IntOverflowRebuilt.java
+++ src/main/java/net/itsky/devoxx/wycocawr/IntOverflowRebuilt.java
@@ -1,17 +1,14 @@
 package net.itsky.devoxx.wycocawr;
 
-import static net.itsky.devoxx.wycocawr.NormalizedLong.iadd;
-import static net.itsky.devoxx.wycocawr.NormalizedLong.imultiply;
-
-import net.itsky.devoxx.wycocawr.NormalizationUtils.iadd;
-import net.itsky.devoxx.wycocawr.NormalizationUtils.imultiply;
+import static net.itsky.devoxx.wycocawr.NormalizationUtils.iadd;
+import static net.itsky.devoxx.wycocawr.NormalizationUtils.imultiply;
 
 public class IntOverflowRebuilt {
 
 	public static void main(String[] args) {
 		long x = 2_000_000_000;
-		long y = NormalizationUtils.iadd(x, x);
-		long z = NormalizationUtils.imultiply(x, x);
+		long y = iadd(x, x);
+		long z = imultiply(x, x);
 		System.out.println("x=" + x + " y=" + y + " z=" + z);
 	}
 }
